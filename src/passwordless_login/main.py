3395951e9a7579cdf161e04e15ff86ff096edd9a
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2023-04-09 15:17:23 +0300

    Installed the fastapi module and configured the templates

--- src/passwordless_login/main.py
+++ src/passwordless_login/main.py
@@ -1,9 +1,33 @@
 
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from pathlib import Path
+
+BASE_PATH = Path(__file__).parent
+STATIC_FOLDER = BASE_PATH.joinpath("static")
+TEMPLATES_FOLDER = BASE_PATH.joinpath("templates")
+
+print(STATIC_FOLDER)
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory=STATIC_FOLDER), name="static")
+
+templates = Jinja2Templates(directory=TEMPLATES_FOLDER)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def homepage(request: Request):
+    context = {
+        "request": request
+    }
+    return templates.TemplateResponse("index.html", context)
+
+
 def run():
-    print("hello world")
     return 0
 
 
 if __name__ == "__main__":
     exit(run())
-
